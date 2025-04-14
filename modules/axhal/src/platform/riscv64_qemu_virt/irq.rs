e591dcd00be58bce0a73aea456f7264d168064a5
aarkegz <aarkegz@gmail.com>
2025-04-14 16:33:29 +0800

    formatted

--- modules/axhal/src/platform/riscv64_qemu_virt/irq.rs
+++ modules/axhal/src/platform/riscv64_qemu_virt/irq.rs
@@ -3,6 +3,40 @@
 use crate::irq::IrqHandler;
 use lazyinit::LazyInit;
 use riscv::register::sie;
+// use riscv_plic::{HartContext, InterruptSource, Plic};
+use sbi_rt::{HartMask, send_ipi};
+
+use memory_addr::{PhysAddr, pa};
+
+use crate::cpu::this_cpu_id;
+use crate::irq::IrqHandler;
+use crate::mem::phys_to_virt;
+
+// struct Context {
+//     hart_id: usize,
+// }
+
+// impl HartContext for Context {
+//     fn index(self) -> usize {
+//         self.hart_id * 2 + 1
+//     }
+// }
+
+// impl Context {
+//     const fn new(hart_id: usize) -> Self {
+//         Self { hart_id }
+//     }
+// }
+
+// struct Interrupt {
+//     irq_num: usize,
+// }
+
+// impl InterruptSource for Interrupt {
+//     fn id(self) -> core::num::NonZeroU32 {
+//         core::num::NonZeroU32::new(self.irq_num as u32).unwrap()
+//     }
+// }
 
 /// `Interrupt` bit in `scause`
 pub(super) const INTC_IRQ_BASE: usize = 1 << (usize::BITS - 1);
